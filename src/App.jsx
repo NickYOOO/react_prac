40c59f435ed3acd70a53f1af56fdbb7c6079ace9
Nick YOOO <outgrowthr@gmail.com>
2023-06-13 14:36:55 +0900

    Study: map, fillter 가지고 List CRD 기능구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,89 @@
 import React from "react";
 import 'App.css'
+import { useState } from "react";
+import matchers from "@testing-library/jest-dom/matchers";
 
 const App = () => {
 
-  const vegetabe = ['감자', '고구마', '오이', '가지', '옥수수'];
+  const [users, setUsers] = useState([
+    { id: 1, age: 30, name: "송중기" },
+    { id: 2, age: 24, name: "송강" },
+    { id: 3, age: 25, name: "김유정" },
+    { id: 4, age: 33, name: "구교환" },
+    { id: 5, age: 32, name: "마동석" },
+  ]);
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+
+  const nameChangeHandler = (e) => {
+    setName(e.target.value);
+  }
+
+  const ageChangeHandler = (e) => {
+    setAge(e.target.value);
+  }
+
+  // 추가 버튼
+  const clickButtonHandler = () => {
+    const newUser = {
+      id: users.length + 1,
+      age: age,
+      name: name,
+    };
+
+    setUsers([...users, newUser]);
+  }
+
+  // 삭제 버튼
+  const clickDeleteHandler = (id) => {
+    const newUsers = users.filter(user => user.id !== id);
+    setUsers(newUsers);
+  };
 
   return (
-    <div className="app-style">
-      {
-        vegetabe.filter((item) => {
-          return item !== "오이"
-        })
-          .map((item) => {
-            return <div className="squareStyle">{item}</div>
+    <>
+      <div>
+        이름:&nbsp;
+        <input
+          value={name}
+          onChange={nameChangeHandler}
+        />
+        <br />
+        나이:&nbsp;
+        <input
+          value={age}
+          onChange={ageChangeHandler}
+        />
+        <br />
+        <button onClick={clickButtonHandler}>추가</button>
+      </div>
+
+      <div className="app-style">
+        {
+          users.map((item) => {
+            return (
+              <User
+                key={item.id}
+                item={item}
+                deleteFunction={clickDeleteHandler} />
+            );
           })
-      }
-    </div>
+        }
+      </div>
+    </>
 
   );
 };
+
+const User = ({ item, deleteFunction }) => {
+
+  return (
+    <div key={item.id} className="squareStyle">
+      {item.age} - {item.name}
+      <button onClick={() => deleteFunction(item.id)}>삭제</button>
+    </div>
+  )
+}
 
 export default App;
