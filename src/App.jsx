abb5306af3f8149f68530274b2243b2ba8612bff
Nick YOOO <outgrowthr@gmail.com>
2023-06-13 09:25:24 +0900

    Study: State 기초

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import React from 'react';
-import Child from 'Child';
+
 
 function App() {
+  // state를 쓰는 목적!
+  // UI를 바꾸기 위해서!
+
+  // hook
+  // const [state, setState] = useState('initial Value');
+
+  const [name, setName] = useState('김할아버지');
   return (
-    <Child age={20} name={name}>
-      이름
-    </Child>
+    <div>
+      {name}
+      <br />
+      <button
+        onClick={function () {
+          setName('김할머니')
+        }}
+      >클릭</button>
+    </div>
   );
 }
 export default App;
