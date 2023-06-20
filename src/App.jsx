27f8d679cae609ea433071f821712e94efde7b34
Nick YOOO <outgrowthr@gmail.com>
2023-06-20 16:56:34 +0900

    Study: GlobalStyle 전역 스타일 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import TestPage from "components/TestPage";
 import React from "react";
 import { styled } from "styled-components";
-
+import GlobalStyle from "./GlobalStyle"
 
 const App = () => {
 
@@ -23,26 +24,26 @@ const App = () => {
 
   return (
     <>
-      {/* <StBox borderColor="red" >빨간 박스</StBox> */}
-      {
-        boxList.map((box) => {
-          return <StBox borderColor={box} >{getBoxName(box)}</StBox>
-        })
-      }
-
+      <GlobalStyle />
+      <TestPage
+        title="제목입니다."
+        contents="내용입니다."
+      />
     </>
+
+
   )
-};
+}
 
 export default App;
 
 const StContainer = styled.div`
-  display: flex;
-`;
+      display: flex;
+      `;
 
 const StBox = styled.div`
-width: 100px;
-height: 100px;
-border: 1px solid ${(props) => props.borderColor};
-margin: 20px;
-`
+      width: 100px;
+      height: 100px;
+      border: 1px solid ${(props) => props.borderColor};
+      margin: 20px;
+      `
