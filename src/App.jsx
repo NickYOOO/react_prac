69a67d508a2ac74ed835719f81dd026f3d27f365
Nick YOOO <outgrowthr@gmail.com>
2023-06-21 10:55:24 +0900

    Study: useCallback 함

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,22 @@ function App() {
     setCount(count - 1);
   };
 
+  // count를 초기화해주는 함수
+
+  // 변경 전
+  // const initCount = () => {
+  //   setCount(0);
+  // };
+
+  // 변경 후
+  // count를 초기화해주는 함수
+  const initCount = useCallback(() => {
+    console.log(`[COUNT 변경] ${count}에서 0으로 변경되었습니다.`);
+    setCount(0);
+  }, [count]);
+
+
+
   return (
     <>
       <h3>카운트 예제입니다!</h3>
@@ -30,7 +46,7 @@ function App() {
       <button onClick={onPlusButtonClickHandler}>+</button>
       <button onClick={onMinusButtonClickHandler}>-</button>
       <div style={boxesStyle}>
-        <Box1 />
+        <Box1 initCount={initCount} />
         <Box2 />
         <Box3 />
       </div>
