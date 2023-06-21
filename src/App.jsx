48b239faaba65eced7b6d722d007888f72d6a12b
Nick YOOO <outgrowthr@gmail.com>
2023-06-21 10:22:52 +0900

    Study: useEffect,[비어있는 의존성 배열] :

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import GrandFather from "components/GrandFather";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [value, setValue] = useState("");
+  useEffect(() => {
+    console.log("hello useEffect");
+  }, []); // 비어있는 의존성 배열
 
   return (
-    <>
-      <GrandFather />
-    </>
-
-
-  )
+    <div>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
-
