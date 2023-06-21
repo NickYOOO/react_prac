9427a0d1dd9b838a574f2e5d0584c0d7da560916
Nick YOOO <outgrowthr@gmail.com>
2023-06-21 10:24:32 +0900

    Study: useEffect, [clean up]

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
 // src/App.js
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 const App = () => {
-  const [value, setValue] = useState("");
-  useEffect(() => {
-    console.log("hello useEffect");
-  }, [value]); // value를 넣음
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={value}
-        onChange={(event) => {
-          setValue(event.target.value);
-        }}
-      />
-    </div>
-  );
-}
+  useEffect(() => {
+    // 화면에 컴포넌트가 나타났을(mount) 때 실행하고자 하는 함수를 넣어주세요.
+
+    return () => {
+      // 화면에서 컴포넌트가 사라졌을(unmount) 때 실행하고자 하는 함수를 넣어주세요.
+    }
+  }, [])
+
+  return <div>hello react!</div>
+};
 
 export default App;
