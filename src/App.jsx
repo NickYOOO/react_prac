70752a4a8b841b52e2f0291306a2005b424a85ca
Nick YOOO <outgrowthr@gmail.com>
2023-06-21 10:49:44 +0900

    Study: React.memo 컴포넌트를 메모리제이션!

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-import { useEffect, useRef } from "react";
-import "./App.css";
+import React, { useState } from "react";
+import Box1 from "./components/Box1";
+import Box2 from "./components/Box2";
+import Box3 from "./components/Box3";
+
+const boxesStyle = {
+  display: "flex",
+  marginTop: "10px",
+};
 
 function App() {
-  const idRef = useRef("");
+  console.log("App 컴포넌트가 렌더링되었습니다!");
 
-  // 렌더링이 될 때
-  useEffect(() => {
-    idRef.current.focus();
-  }, []);
+  const [count, setCount] = useState(0);
+
+  // 1을 증가시키는 함수
+  const onPlusButtonClickHandler = () => {
+    setCount(count + 1);
+  };
+
+  // 1을 감소시키는 함수
+  const onMinusButtonClickHandler = () => {
+    setCount(count - 1);
+  };
 
   return (
     <>
-      <div>
-        아이디 : <input type="text" ref={idRef} />
-      </div>
-      <div>
-        비밀번호 : <input type="password" />
+      <h3>카운트 예제입니다!</h3>
+      <p>현재 카운트 : {count}</p>
+      <button onClick={onPlusButtonClickHandler}>+</button>
+      <button onClick={onMinusButtonClickHandler}>-</button>
+      <div style={boxesStyle}>
+        <Box1 />
+        <Box2 />
+        <Box3 />
       </div>
     </>
   );
