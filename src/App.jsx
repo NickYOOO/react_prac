7b5f5607c55c9831fff1df372ff138de43db5e63
Nick YOOO <outgrowthr@gmail.com>
2023-06-21 10:14:52 +0900

    Study: props vs useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
-import TestPage from "components/TestPage";
+import GrandFather from "components/GrandFather";
 import React from "react";
-import { styled } from "styled-components";
-import GlobalStyle from "./GlobalStyle"
 
 const App = () => {
 
-  // 박스의 색을 배열에 담습니다.
-  const boxList = ["red", "green", "blue", "black"];
-
-  // 색을 넣으면, 이름을 반환해주는 함수를 만듭니다.
-  const getBoxName = (color) => {
-    switch (color) {
-      case "red":
-        return "빨간 박스";
-      case "green":
-        return "초록 박스";
-      case "blue":
-        return "파란 박스";
-      default:
-        return "검정 박스";
-    }
-  }
-
   return (
     <>
-      <GlobalStyle />
-      <TestPage
-        title="제목입니다."
-        contents="내용입니다."
-      />
+      <GrandFather />
     </>
 
 
@@ -37,13 +14,3 @@ const App = () => {
 
 export default App;
 
-const StContainer = styled.div`
-      display: flex;
-      `;
-
-const StBox = styled.div`
-      width: 100px;
-      height: 100px;
-      border: 1px solid ${(props) => props.borderColor};
-      margin: 20px;
-      `
---
+++ src/components/GrandFather.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Father from './Father';
+
+function GrandFather() {
+    const houseName = '스파르타';
+    const pocketMoney = "10000"
+    return (
+        <Father houseName={houseName} pocketMoney={pocketMoney} />
+    )
+}
+
+export default GrandFather
